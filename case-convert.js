9a1c33c376c4d4a7de721e70a17ddb4838e3622a
NishatFariza <nishatfarzia78@gmail.com>
2022-02-15 02:14:00 +0600

    case convert practice done

--- case-convert.js
+++ case-convert.js
@@ -1,1 +1,8 @@
 const anthem ='Amar Sonar Bangla Ami Tomay Valobashi';
+
+
+const userInput ='bLaCkPink';
+const saveUserName ='blackPink';
+if(userInput.toLowerCase() == saveUserName.toLowerCase()){
+    console.log('user exist');
+}
